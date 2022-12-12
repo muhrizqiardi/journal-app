e499cf3e318eee71fc55065acabcc0291b19966c
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-12-12 18:11:17 +0700

    feat: use middleware function from `next-auth`

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname == "/") {
-    if (!request.headers.has("Authorization"))
-      return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (request.nextUrl.pathname.startsWith("/api/entries")) {
-    if (!request.headers.has("Authorization"))
-      return NextResponse.rewrite(new URL("/api/401", request.url));
-  }
-
-  if (request.nextUrl.pathname.startsWith("/api/users")) {
-    if (request.method === "POST") return NextResponse.next();
-
-    if (!request.headers.has("Authorization"))
-      // no longer works
-      // return NextResponse.json({ message: "Auth required" }, { status: 401 });
-      return NextResponse.rewrite(new URL("/api/401", request.url));
-  }
-
-  return NextResponse.next();
-}
+export const config = { matcher: ["/"] };
