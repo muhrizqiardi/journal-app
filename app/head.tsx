d85f9f9b5e11f720d37e8d87d7b3bd25f9321db0
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-12-13 13:44:12 +0700

    feat: add metatags

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,28 @@ export default function Head() {
   return (
     <>
       <title>Journal App</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      {/* Primary Meta Tags */}
+      <meta name="title" content="Journal App" />
+      <meta
+        name="description"
+        content="Create journal entries and track your mood!"
+      />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://journal-app-lilac.vercel.app/" />
+      <meta property="og:title" content="Journal App" />
+      <meta
+        property="og:description"
+        content="Create journal entries and track your mood!"
+      />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://journal-app-lilac.vercel.app/" />
+      <meta property="twitter:title" content="Journal App" />
+      <meta
+        property="twitter:description"
+        content="Create journal entries and track your mood!"
+      />
     </>
-  )
+  );
 }
