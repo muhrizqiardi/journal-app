e169ac5bfbc480f99e097cafd914fefbe0bd777a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-12-17 16:12:26 +0700

    fix: fix mood ranges

--- components/MoodNumberToEmoji.tsx
+++ components/MoodNumberToEmoji.tsx
@@ -1,7 +1,7 @@
 export default function MoodNumberToEmoji({ mood }: { mood: number }) {
   if (mood === 1) return <>😁</>;
-  if (mood === 0.75) return <>🙂</>;
-  if (mood === 0.25) return <>🙁</>;
-  if (mood === 0) return <>😞</>;
+  if (mood >= 0.75 && mood < 1) return <>🙂</>;
+  if (mood > 0.75 && mood < 0.5) return <>🙁</>;
+  if (mood < 0.25) return <>😞</>;
   return <>-</>;
 }
